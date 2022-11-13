7039cd041171709f5c9f2f34259cc8b66df50f8c
marvin <trujillomarvin@hotmail.com>
2022-11-13 11:26:15 -0600

    changes in imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
+
+  
   return (
     <div className={styles.container}>
       <Head>
