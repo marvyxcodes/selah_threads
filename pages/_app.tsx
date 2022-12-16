1807e601c0912b10c92fceb770d2d3852830403d
Marvin <trujillomarvin@hotmail.com>
2022-12-16 15:30:17 -0600

    shopping cart implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import { SessionProvider } from "next-auth/react";
+import { ShoppingCartProvider } from "../context/ShoppingCartContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <SessionProvider>
-        <Navbar />
-        <Component {...pageProps} />
+        <ShoppingCartProvider>
+          <Navbar />
+          <Component {...pageProps} />
+        </ShoppingCartProvider>
       </SessionProvider>
     </>
   );
